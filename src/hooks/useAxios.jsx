b8d00e1f49933a232697f6f2c4d358bc13e5b1f7
MD-Sazzadur-Rahman-Yamin <mailme.yamin@gmail.com>
2025-11-13 23:04:51 +0600

    finish

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,12 +1,23 @@
 import axios from "axios";
+// import useAuth from "./useAuth";
+// import { useEffect } from "react";
 
 const instance = axios.create({
   baseURL: "http://localhost:3333",
-}); 
-
+});
 
 const useAxios = () => {
-  const axiosH = instance
-    return { axiosH };
+  // const { user } = useAuth();
+  const axiosH = instance;
+  // useEffect(() => {
+  //   const axiosSecure = instance.interceptors.request.use((config) => {
+  //     config.headers.authorization = `Bearer ${user}`;
+  //     return config;
+  //   });
+  //   return ()=>{
+  //     instance.interceptors.request.eject(axiosSecure)
+  //   }
+  // }, [user]);
+  return { axiosH };
 };
 export default useAxios;
